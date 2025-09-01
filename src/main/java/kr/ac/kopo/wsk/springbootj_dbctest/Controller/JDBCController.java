2967069a8324ef073f7368453c17e454c615dbc0
wshin025 <wody0506@gmail.com>
2025-09-01 11:42:46 +0900

    JDBC2강 수정,추가,삭제 그리고 (환경설정)

--- src/main/java/kr/ac/kopo/wsk/springbootj_dbctest/Controller/JDBCController.java
+++ src/main/java/kr/ac/kopo/wsk/springbootj_dbctest/Controller/JDBCController.java
@@ -5,8 +5,7 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,8 +18,46 @@ public class JDBCController {
     @GetMapping
     public String requestMethod(Model model) {
         String sql = "select * from person";
-        List<Person> personList =jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Person.class));
+        List<Person> personList = jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Person.class));
         model.addAttribute("personList", personList);
-        return"viewPage01";
+        return "viewPage01";
+    }
+
+    @GetMapping("/new")
+    public String newMethod(Model model) {
+        Person person = new Person();
+        model.addAttribute("person", person);
+        return "viewPage01_new";
+    }
+
+    @PostMapping("/insert")
+    public String insertMethod(@ModelAttribute Person person) {
+        String sql = "insert into person (name, age, email) values (?, ?, ?)";
+//        jdbcTemplate.update(sql, person.getName(), person.getAge(), person.getEmail());
+        Object[] params = {person.getName(), person.getAge(), person.getEmail()};
+        int resultCount = jdbcTemplate.update(sql, params);
+        return "redirect:/exam01";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editMethod(@PathVariable int id, Model model) {
+        String sql = "select * from person where id = ?";
+        Person person = jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(Person.class), id);
+        model.addAttribute("person", person);
+        return "viewPage01_edit01";
+    }
+    @PostMapping("/update")
+    public String updateMethod(@ModelAttribute Person person) {
+        String sql = "update person set name = ?, age = ?, email = ? where id = ?";
+        Object[] params = {person.getName(), person.getAge(), person.getEmail(), person.getId()};
+        int resultCount = jdbcTemplate.update(sql, params);
+        return "redirect:/exam01";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteMethod(@PathVariable int id) {
+        String sql = "delete from person where id = ?";
+        int resultCount = jdbcTemplate.update(sql, id);
+        return "redirect:/exam01";
     }
 }
